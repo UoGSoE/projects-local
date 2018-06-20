07175a77e9192fe9d187efb87c0b74b8d99a7650
Billy <william.allan@glasgow.ac.uk>
2018-06-20 14:33:45 +0100

    See if laughable attempt at building a docker image works

--- docker/build.sh
+++ docker/build.sh
@@ -16,11 +16,11 @@ HASH=$(git rev-parse HEAD)
 cd $DIR/../
 # "archive" gives us useful tools - we can use .gitattributes
 # to `export-ignore` extraneous files
-git archive --format=tar --worktree-attributes $HASH | tar -xf - -C $DIR/docker/packaged
+git archive --format=tar --worktree-attributes $HASH | tar -xf - -C $DIR/packaged
 
 # Production Build Steps
 ## (Decision between export-ignore'ing docker/develop command or not)
-cd $DIR/docker/packaged
+cd $DIR/packaged
 composer install --no-dev
 npm install && npm run prod && rm -fr node_modules
 
@@ -31,19 +31,18 @@ cp .env.example .env
 php artisan key:generate
 
 # Build the Docker image with latest code
-cd $DIR/docker
+cd $DIR/packaged
+cp ../Dockerfile ./
 docker build \
-    -t glasgow-projects/web:latest \
-    -t glasgow-projects/web:$HASH .
+    -t localhost:5000/glasgow-projects-app:latest \
+    -t localhost:5000/glasgow-projects-app:$HASH .
+docker push localhost:5000/glasgow-projects-app:$HASH
+docker push localhost:5000/glasgow-projects-app:latest
 
 # Clean up packaged directory
-cd $DIR/docker/packaged
+cd $DIR/packaged
 PWD=$(pwd)
-if [ "$PWD" == "$DIR/docker/packaged" ]; then
-    # The "vendor" directory (any any built assets!) will be owned
-    # as user "root" on the Linux file system
-    # So we'll use Docker to delete them with a one-off container
-
-    #docker run --rm -w /opt -v $(pwd):/opt ubuntu:16.04 bash -c "rm -rf ./* && rm -rf ./.git* && rm .env*"
-    #touch .gitkeep
+if [ "$PWD" == "$DIR/packaged" ]; then
+    rm -rf ./* && rm -rf ./.git* && rm .env* && rm .dockerignore
+    touch .gitkeep
 fi
