70ec738f5f209ccac274f253681c61d0aad40e60
Billy <william.allan@glasgow.ac.uk>
2018-06-20 13:46:31 +0100

    Work on build script

--- docker/build.sh
+++ docker/build.sh
@@ -16,32 +16,34 @@ HASH=$(git rev-parse HEAD)
 cd $DIR/../
 # "archive" gives us useful tools - we can use .gitattributes
 # to `export-ignore` extraneous files
-git archive --format=tar --worktree-attributes $HASH | tar -xf -  -C $DIR/app/packaged
+git archive --format=tar --worktree-attributes $HASH | tar -xf - -C $DIR/docker/packaged
 
 # Production Build Steps
 ## (Decision between export-ignore'ing docker/develop command or not)
-cd $DIR/app/packaged
-./develop composer install --no-dev
-# ./develop yarn install
-# ./develop gulp --production
+cd $DIR/docker/packaged
+composer install --no-dev
+npm install && npm run prod && rm -fr node_modules
 
 # Get the production .env file
 ## This assumes we're running in Jenkins as user "jenkins"
-/var/lib/jenkins/.venv/bin/aws s3 cp s3://shippingdocker-secrets/env-prod .env
+#/var/lib/jenkins/.venv/bin/aws s3 cp s3://shippingdocker-secrets/env-prod .env
+cp .env.example .env
+php artisan key:generate
 
 # Build the Docker image with latest code
-cd $DIR/app
+cd $DIR/docker
 docker build \
-    -t shippingdocker.com/app:latest \
-    -t shippingdocker.com/app:$HASH .
+    -t glasgow-projects/web:latest \
+    -t glasgow-projects/web:$HASH .
 
 # Clean up packaged directory
-cd $DIR/app/packaged
+cd $DIR/docker/packaged
 PWD=$(pwd)
-if [ "$PWD" == "$DIR/app/packaged" ]; then
-    # The "vendor" directory (any any built assets!) will be owned 
+if [ "$PWD" == "$DIR/docker/packaged" ]; then
+    # The "vendor" directory (any any built assets!) will be owned
     # as user "root" on the Linux file system
     # So we'll use Docker to delete them with a one-off container
-    docker run --rm -w /opt -v $(pwd):/opt ubuntu:16.04 bash -c "rm -rf ./* && rm -rf ./.git* && rm .env*"
-    touch .gitkeep
+
+    #docker run --rm -w /opt -v $(pwd):/opt ubuntu:16.04 bash -c "rm -rf ./* && rm -rf ./.git* && rm .env*"
+    #touch .gitkeep
 fi
