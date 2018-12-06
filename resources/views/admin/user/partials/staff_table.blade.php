c631ee561a84bae4aa9e9ecb84fdac5696443746
Billy <william.allan@glasgow.ac.uk>
2018-12-06 13:12:46 +0000

    Switched to the filterable-items on user index pages

--- resources/views/admin/user/partials/staff_table.blade.php
+++ resources/views/admin/user/partials/staff_table.blade.php
@@ -1,44 +1,33 @@
-
-<table-component
-    :data='@json($users)'
-    sort-by="surname"
-    sort-order="asc"
-    table-class="table is-fullwidth is-striped is-hover"
-    :show-caption="false"
-    filter-input-class="input"
-    >
-    <table-column show="username" label="User">
-        <template slot-scope="row">
-            <a :href="showUserUrl(row.id)">@{{ row.username }}</a>
-        </template>
-    </table-column>
-    <table-column show="surname" label="Surname"></table-column>
-    <table-column show="forenames" label="Forename"></table-column>
-    <table-column show="ugrad_active" label="Ugrad Proj">
-        <template slot-scope="row">
-            @{{ row.ugrad_active }} / @{{ row.ugrad_allocated}}
-        </template>
-    </table-column>
-    <table-column show="pgrad_active" label="Pgrad Proj">
-        <template slot-scope="row">
-            @{{ row.pgrad_active }} / @{{ row.pgrad_allocated}}
-        </template>
-    </table-column>
-
-    <table-column show="second_ugrad_active" label="2nd Ugrad Proj">
-        <template slot-scope="row">
-            @{{ row.second_ugrad_active }} / @{{ row.second_ugrad_allocated}}
-        </template>
-    </table-column>
-    <table-column show="second_pgrad_active" label="2nd Pgrad Proj">
-        <template slot-scope="row">
-            @{{ row.second_pgrad_active }} / @{{ row.second_pgrad_allocated}}
-        </template>
-    </table-column>
-
-    <table-column show="email" label="Email">
-        <template slot-scope="row">
-            <a :href="`mailto:${row.email}`">@{{ row.email }}</a>
-        </template>
-    </table-column>
-</table-component>
+<filterable-items :items='@json($users)'>
+    <span  slot-scope="{ items: staffMembers, inputAttrs, inputEvents, sortOn }">
+        <input class="input" type="text" v-bind="inputAttrs" v-on="inputEvents" placeholder="Filter table..." autofocus>
+        <table class="table is-fullwidth is-striped is-hover">
+            <thead>
+                <tr>
+                    <th @click.prevent="sortOn('username')" class="cursor-pointer">User</th>
+                    <th @click.prevent="sortOn('surname')" class="cursor-pointer">Surname</th>
+                    <th @click.prevent="sortOn('forenames')" class="cursor-pointer">Forenames</th>
+                    <th @click.prevent="sortOn('ugrad_active')" class="cursor-pointer">Ugrad Proj</th>
+                    <th @click.prevent="sortOn('pgrad_active')" class="cursor-pointer">Pgrad Proj</th>
+                    <th @click.prevent="sortOn('second_ugrad_active')" class="cursor-pointer">2nd Ugrad Proj</th>
+                    <th @click.prevent="sortOn('second_pgrad_active')" class="cursor-pointer">2nd Pgrad Proj</th>
+                    <th @click.prevent="sortOn('email')" class="cursor-pointer">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="staff in staffMembers" :key="staff.id">
+                    <td>
+                        <a :href="showUserUrl(staff.id)">@{{ staff.username }}</a>
+                    </td>
+                    <td>@{{ staff.surname }}</td>
+                    <td>@{{ staff.forenames }}</td>
+                    <td>@{{ staff.ugrad_active }} / @{{ staff.ugrad_allocated}}</td>
+                    <td>@{{ staff.pgrad_active }} / @{{ staff.pgrad_allocated}}</td>
+                    <td>@{{ staff.second_ugrad_active }} / @{{ staff.second_ugrad_allocated}}</td>
+                    <td>@{{ staff.second_pgrad_active }} / @{{ staff.second_pgrad_allocated}}</td>
+                    <td><a :href="`mailto:${staff.email}`">@{{ staff.email }}</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </span>
+</filterable-items>
--- resources/views/admin/user/partials/student_table.blade.php
+++ resources/views/admin/user/partials/student_table.blade.php
@@ -1,22 +1,25 @@
-
-<table-component
-    :data='@json($users)'
-    sort-by="surname"
-    sort-order="asc"
-    table-class="table is-fullwidth is-striped is-hover"
-    :show-caption="false"
-    filter-input-class="input"
-    >
-    <table-column show="username" label="User">
-        <template slot-scope="row">
-            <a :href="showUserUrl(row.id)">@{{ row.username }}</a>
-        </template>
-    </table-column>
-    <table-column show="surname" label="Surname"></table-column>
-    <table-column show="forenames" label="Forename"></table-column>
-    <table-column show="email" label="Email">
-        <template slot-scope="row">
-            <a :href="`mailto:${row.email}`">@{{ row.email }}</a>
-        </template>
-    </table-column>
-</table-component>
+<filterable-items :items='@json($users)'>
+    <span  slot-scope="{ items: students, inputAttrs, inputEvents, sortOn }">
+        <input class="input" type="text" v-bind="inputAttrs" v-on="inputEvents" placeholder="Filter table..." autofocus>
+        <table class="table is-fullwidth is-striped is-hover">
+            <thead>
+                <tr>
+                    <th @click.prevent="sortOn('username')" class="cursor-pointer">User</th>
+                    <th @click.prevent="sortOn('surname')" class="cursor-pointer">Surname</th>
+                    <th @click.prevent="sortOn('forenames')" class="cursor-pointer">Forenames</th>
+                    <th @click.prevent="sortOn('email')" class="cursor-pointer">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="student in students" :key="student.id">
+                    <td>
+                        <a :href="showUserUrl(student.id)">@{{ student.username }}</a>
+                    </td>
+                    <td>@{{ student.surname }}</td>
+                    <td>@{{ student.forenames }}</td>
+                    <td><a :href="`mailto:${student.email}`">@{{ student.email }}</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </span>
+</filterable-items>
