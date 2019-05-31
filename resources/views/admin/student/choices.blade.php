fda753f5604bca5a91514f20c1f6826a29bdadca
Billy <william.allan@glasgow.ac.uk>
2019-05-31 09:41:52 +0100

    Added a little max students indication on bulk allocation/choices page

--- resources/views/admin/student/choices.blade.php
+++ resources/views/admin/student/choices.blade.php
@@ -9,19 +9,19 @@
 <form method="POST" action="{{ route('project.bulk_accept') }}">
     @csrf
 
-<table class="table is-fullwidth is-striped is-hoverable">
-    <thead>
-        <tr>
-            <th>Student</th>
-            <th>1st</th>
-            <th>2nd</th>
-            <th>3rd</th>
-            <th>4th</th>
-            <th>5th</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($students as $student)
+    <table class="table is-fullwidth is-striped is-hoverable">
+        <thead>
+            <tr>
+                <th>Student</th>
+                <th>1st</th>
+                <th>2nd</th>
+                <th>3rd</th>
+                <th>4th</th>
+                <th>5th</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($students as $student)
             <tr>
                 <td>
                     <a href="{{ route('admin.user.show', $student->id) }}">
@@ -29,25 +29,26 @@
                     </a>
                 </td>
                 @foreach (range(1, config('projects.required_choices')) as $choice)
-                    <td>
-                        @if ($student->projects->where('pivot.choice', $choice)->isNotEmpty())
-                            @if ($student->projects->where('pivot.choice', $choice)->first()->pivot->is_accepted)
-                                &#10003;
-                            @else
-                                <input type="radio" name="students[{{ $student->id}}]" value="{{ $student->projects->where('pivot.choice', $choice)->first()->id }}">
-                            @endif
-                            {{ $student->projects->where('pivot.choice', $choice)->first()->title }}
-                        @endif
-                    </td>
+                <td>
+                    @if ($student->projects->where('pivot.choice', $choice)->isNotEmpty())
+                    @if ($student->projects->where('pivot.choice', $choice)->first()->pivot->is_accepted)
+                    &#10003;
+                    @else
+                    <input type="radio" name="students[{{ $student->id}}]" value="{{ $student->projects->where('pivot.choice', $choice)->first()->id }}">
+                    @endif
+                    {{ $student->projects->where('pivot.choice', $choice)->first()->title }}
+                    (max {{ $student->projects->where('pivot.choice', $choice)->first()->max_students }})
+                    @endif
+                </td>
                 @endforeach
             </tr>
-        @endforeach
-    </tbody>
-</table>
+            @endforeach
+        </tbody>
+    </table>
 
-<div class="field">
-    <div class="control"><button class="button">Save</button></div>
-</div>
+    <div class="field">
+        <div class="control"><button class="button">Save</button></div>
+    </div>
 
 </form>
 @endsection
