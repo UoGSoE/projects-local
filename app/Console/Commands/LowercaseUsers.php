8607c2007865379af42353447080114ece749657
Billy <william.allan@glasgow.ac.uk>
2020-06-29 13:38:53 +0100

    Added artisan command to update all user records to lowercase username & email
    Not for use in this - but will copy'n'paste it to other projects to make up for
    the LDAP issues and changes to the LDAP auth package in v2

---
+++ app/Console/Commands/LowercaseUsers.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class LowercaseUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lowercase:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Convert all user usernames & emails to lowercase';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        User::all()->each(function ($user) {
+            $user->update([
+                'username' => strtolower($user->username),
+                'email' => strtolower($user->email),
+            ]);
+        });
+    }
+}
---
+++ tests/Feature/LowerCaseUserRecordsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LowerCaseUserRecordsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function we_can_convert_all_usernames_and_emails_to_lowercase()
+    {
+        $user1 = create(User::class, ['username' => 'FRED', 'email' => 'FRED@example.com']);
+        $user2 = create(User::class, ['username' => 'JimMY', 'email' => 'JiMMy@EXample.com']);
+
+        $this->artisan('lowercase:users');
+
+        $this->assertEquals('fred', $user1->fresh()->username);
+        $this->assertEquals('fred@example.com', $user1->fresh()->email);
+        $this->assertEquals('jimmy', $user2->fresh()->username);
+        $this->assertEquals('jimmy@example.com', $user2->fresh()->email);
+    }
+}
