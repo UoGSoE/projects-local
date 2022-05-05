7785d48b5a9d67ce7684fbee931c4e62b45ba8b4
Shift <shift@laravelshift.com>
2022-05-05 09:51:29 +0000

    Adopt anonymous migrations

--- database/migrations/2018_01_25_130035_create_project_courses_table.php
+++ database/migrations/2018_01_25_130035_create_project_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectCoursesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreateProjectCoursesTable extends Migration
     {
         Schema::dropIfExists('project_courses');
     }
-}
+};
